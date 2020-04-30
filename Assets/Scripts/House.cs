073bb6f0aa9c06ce3822ad447aee58b18399d343
puspanegara <54850975+puspanegara@users.noreply.github.com>
2020-04-30 18:14:14 +0700

    Okehlah
    Masih ada beberapa kendala sebetulnya

--- Assets/Scripts/House.cs
+++ Assets/Scripts/House.cs
@@ -5,16 +5,20 @@ using UnityEngine.UI;
 
 public class House : MonoBehaviour
 {
-    public string buttonUse;
-    public int countHouseGet;
-    public Text countHouseText; 
+    static int scoreP1;
+    static int scoreP2;
+    public Text textScorePemain1;
+    public Text textScorePemain2; 
     private HouseCek houseCek;
-    public GameObject imageCek;
     public Sprite CheckImage;
+    public GameObject image;
+    
+    //public bool diKunjungi;
     // Start is called before the first frame update
     void Start()
     {
-        houseCek = GameObject.FindGameObjectWithTag("Player").GetComponent<HouseCek>();
+        //houseCek = GameObject.FindGameObjectWithTag("Player","Player2").GetComponent<HouseCek>();
+        houseCek =GameObject.FindGameObjectWithTag("Pengecekan").GetComponent<HouseCek>();
     }
 
     // Update is called once per frame
@@ -23,11 +27,11 @@ public class House : MonoBehaviour
         
     }
 
-    void OnTriggerEnter2D(Collider2D other) 
+    /*void OnTriggerEnter2D(Collider2D other) 
     {
-          if(other.CompareTag("Player"))
-          {
-            for (int i=0; i< houseCek.houses.Length; i++)
+        if(other.CompareTag("Player"))
+        {
+            /*for (int i=0; i< houseCek.houses.Length; i++)
             {
                 if(houseCek.isDone[i] == false)
                 {
@@ -37,7 +41,54 @@ public class House : MonoBehaviour
                     break;
                 }
             }
-            
-          }  
+            if(diKunjungi == false)
+            {
+               scoreP1 ++;
+               diKunjungi = true;
+               Debug.Log(scoreP1);
+               textScorePemain1.text= scoreP1 + "";
+               this.gameObject.GetComponent<SpriteRenderer>().sprite = CheckImage;
+            }
+        }  
+        if (other.CompareTag("Player2"))
+        {
+            if(diKunjungi == false)
+            {
+                scoreP2 ++;
+                //diKunjungi = true;
+                Debug.Log(scoreP2);
+                textScorePemain2.text= scoreP2 + "";
+                this.gameObject.GetComponent<SpriteRenderer>().sprite = CheckImage;
+            }
+        }
+    }*/
+    
+    void OnTriggerEnter2D(Collider2D other) 
+    {
+        for(int i=0; i < houseCek.isDone.Length; i++)
+        {
+            if(houseCek.isDone[i]== false)
+            {
+                if(other.CompareTag("Player"))
+                {
+                    scoreP1 ++;
+                    houseCek.isDone[i] = true; 
+                    Debug.Log(scoreP1);
+                    textScorePemain1.text= scoreP1 + "";
+                    this.gameObject.GetComponent<SpriteRenderer>().sprite = CheckImage;
+                    break;
+                }
+                if(other.CompareTag("Player2"))
+                {
+                        scoreP2 ++;
+                        houseCek.isDone[i]= true;
+                        Debug.Log(scoreP2);
+                        textScorePemain2.text= scoreP2 + "";
+                        this.gameObject.GetComponent<SpriteRenderer>().sprite = CheckImage;
+                        break;
+                }
+            } 
+        }
+        
     }
 }
--- Assets/Scripts/HouseCek.cs
+++ Assets/Scripts/HouseCek.cs
@@ -1,9 +1,19 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class HouseCek : MonoBehaviour
 {
     public bool[] isDone;
     public GameObject[] houses;
+   public GameObject donePanel;
+
+   void Update() 
+   {
+    if(isDone)
+    {
+        donePanel.SetActive(true);
+    }    
+   }
 }
